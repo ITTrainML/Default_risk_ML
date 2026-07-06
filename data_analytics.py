327fec41fe2df7986894f7cf84c3be6cd5d96ced
JoeMarchi <xxxx6712@gmail.com>
2026-07-06 11:47:52 +0800

    新增使用Duckdb進行EDA腳本，另加入gitignore檔

---
+++ data_analytics.py
@@ -0,0 +1,60 @@
+import duckdb
+
+con = duckdb.connect()
+
+# 確認沒有重複case_id
+con.sql("""
+    CREATE OR REPLACE VIEW train_static_0_1_DiffA AS
+    SELECT case_id,count(case_id) as c_case_id
+    FROM 'data/train_static_0_1.parquet'
+    group by case_id
+    having c_case_id >1
+""")
+
+con.sql("""
+    CREATE OR REPLACE VIEW train_static_0_0_DiffA AS
+    SELECT case_id,count(case_id) as c_case_id
+    FROM 'data/train_static_0_0.parquet'
+    group by case_id
+    having c_case_id >1
+""")
+
+con.sql("""
+    CREATE OR REPLACE VIEW train_base_DiffA AS
+    SELECT case_id,count(case_id) as c_case_id
+    FROM 'data/train_base.parquet'
+    group by case_id
+    having c_case_id >1
+""")
+
+#確認筆數,max
+con.sql("""
+    CREATE OR REPLACE VIEW train_static_0_1 AS
+    SELECT *
+    FROM 'data/train_static_0_1.parquet'
+""")
+
+con.sql("""
+    CREATE OR REPLACE VIEW train_static_0_0 AS
+    SELECT *
+    FROM 'data/train_static_0_0.parquet'
+""")
+
+con.sql("""
+    CREATE OR REPLACE VIEW train_base AS
+    SELECT *
+    FROM 'data/train_base.parquet'
+""")
+
+con.sql("""
+    CREATE OR REPLACE VIEW train_base AS
+    SELECT *
+    FROM 'data/train_base.parquet'
+""")
+
+
+con.sql("CALL start_ui();")
+print(con.sql("SELECT loaded,installed FROM duckdb_extensions() WHERE extension_name = 'ui';"))
+
+#叫出duckui，請在下列程式中設定中斷點
+print("Hellow")
